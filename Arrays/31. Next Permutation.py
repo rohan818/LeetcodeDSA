3ca5be9bc8fed28034e4603f77f6a5d267d3a831
rohan818 <64457330+rohan818@users.noreply.github.com>
2024-10-13 16:21:12 -0400

    code 31

---
+++ Arrays/31. Next Permutation.py
@@ -0,0 +1,17 @@
+#31. Next Permutation
+def nextPermutation(self, nums: List[int]) -> None:
+
+    i = len(nums) - 2
+
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    
+    if i >= 0:
+        j = len(nums) - 1
+        while j >= 0 and nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+    
+    nums[i + 1:] = reversed(nums[i + 1:])
+
+    return nums
