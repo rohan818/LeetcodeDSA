2a4ebe188c047e6415748029ba997569069ba6a9
rohan818 <64457330+rohan818@users.noreply.github.com>
2024-11-21 19:22:02 -0500

    code 128

---
+++ Arrays/128. Longest Consecutive Sequence.py
@@ -0,0 +1,22 @@
+# Longest Consecutive Sequence
+
+class Solution:
+  def longestConsecutive(self, nums: List[int]) -> int:
+      
+      n = len(nums)
+      if n == 0:
+          return 0
+      nums.sort()
+
+      res = 1
+      c = 1
+
+      for i in range(1, n):
+          if nums[i] != nums[i-1]:
+              if nums[i] - nums[i-1] ==1:
+                  c += 1
+              else:
+                  res = max(res, c)
+                  c = 1
+                  
+      return max(res, c)
