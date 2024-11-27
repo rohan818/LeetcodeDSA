350a0ebc7f07f3a7d86f6d577f331295c4120d32
rohan818 <64457330+rohan818@users.noreply.github.com>
2024-11-26 22:17:46 -0500

    code 287

---
+++ Arrays/287. Find the Duplicate Number.py
@@ -0,0 +1,19 @@
+# Find the Duplicate Number
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        low = 1
+        high = len(nums) - 1
+
+        while low <= high:
+            cur = (low + high) // 2
+            count = 0
+
+            count = sum(num <= cur for num in nums)
+            if count > cur:
+                dup = cur
+                high = cur - 1
+            else:
+                low = cur + 1
+
+        return dup
