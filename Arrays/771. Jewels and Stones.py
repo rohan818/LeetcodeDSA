2b58c4032e0c249ea3864ba85e3ce4bc25e90d30
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-01-03 18:04:23 -0500

    code 771

---
+++ Arrays/771. Jewels and Stones.py
@@ -0,0 +1,5 @@
+# Jewels and Stones
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewel_set = set(jewels)
+        return sum(s in jewel_set for s in stones)
