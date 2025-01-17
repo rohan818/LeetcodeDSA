072777846b993f1fbbbeecae1027c1e6227fc723
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-01-17 16:55:48 -0500

    code 1002

---
+++ Strings/1002. Find Common Characters.py
@@ -0,0 +1,12 @@
+# Find Common Characters
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        common_count = Counter(words[0])
+        for word in words [1:]:
+            common_count &= Counter(word)
+
+        result =[]
+        for char, count in common_count.items():
+            result.extend([char] * count)
+        
+        return result
