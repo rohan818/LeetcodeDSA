26d04922942618e1e9606f9cd6564da06f54ce1e
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-01-26 18:01:17 -0500

    code 917

---
+++ Strings/917. Reverse Only Letters.py
@@ -0,0 +1,17 @@
+# Reverse Only Letters
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        i = 0
+        j = len(s) - 1
+        s = [i for i in s]
+        while i <= j:
+            if not s[i].isalpha():
+                i += 1
+                continue
+            if not s[j].isalpha():
+                j -= 1
+                continue
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return "".join(s)
