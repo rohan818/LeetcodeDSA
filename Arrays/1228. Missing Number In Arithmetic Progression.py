65f950261d40158ab8cadcf64f68777034ffa255
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-02-05 17:43:04 -0500

    code 1228

---
+++ Arrays/1228. Missing Number In Arithmetic Progression.py
@@ -0,0 +1,9 @@
+# Missing Number In Arithmetic Progression
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        d = (arr[-1] - arr[0]) // len(arr)
+        if d == 0:
+            return arr[0]
+        for i in range(1, len(arr)):
+            if arr[i-1] + d != arr[i]:
+                return arr[i-1] + d
