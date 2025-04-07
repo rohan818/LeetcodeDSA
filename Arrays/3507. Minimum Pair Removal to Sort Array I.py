6fe7922fe7cf10ac8cbc564f0d9f7ad8eec0de38
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-04-06 20:15:47 -0400

    c 3507

---
+++ Arrays/3507. Minimum Pair Removal to Sort Array I.py
@@ -0,0 +1,24 @@
+# Minimum Pair Removal to Sort Array I
+
+import sys
+class Solution:
+    def minimumPairRemoval(self, nums: List[int]) -> int:
+        if nums == sorted(nums):
+            return 0
+        
+        c = 0
+        while nums != sorted(nums):
+            minSum = sys.maxsize
+            minIndices = [0, 0]
+            for i in range(len(nums)-1):
+                if nums[i]+nums[i+1] < minSum:
+                    minSum = nums[i]+nums[i+1]
+                    minIndices[0] = i
+                    minIndices[1] = i+1
+            #print('^^^', minIndices, nums[minIndices[0]], nums[minIndices[1]])
+            nums[minIndices[0]] = nums[minIndices[0]] + nums[minIndices[1]]
+            nums.pop(minIndices[1])
+            c += 1
+            #print(nums, c)
+        return c
+                
