964892aa26679060f11ab267b6ccb1b0e0dc7986
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-06-10 20:48:15 +0530

    Create 1394.

---
+++ Arrays/1394. Find Lucky Integer in an Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        d = {}
+        for i in arr:
+            d[i] = d.get(i, 0) + 1
+        l = -1
+        for n, count in d.items():
+            if n == count:
+                l = max(l, n)
+        return l
