6e824a49a44528bdac8b4d139ea4a52cea44a696
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-07-28 17:54:36 +0530

    code .243

---
+++ Strings/243. Shortest Word Distance.py
@@ -0,0 +1,13 @@
+def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        w1 = w2  = -1
+        d = n = len(wordsDict)
+        for i in range(n):
+            if wordsDict[i] == word1:
+                w1 = i
+            elif wordsDict[i] == word2:
+                w2 = i
+            
+            if w1 != -1 and w2 != -1:
+                d = min(d, abs(w1 - w2))
+
+        return d
