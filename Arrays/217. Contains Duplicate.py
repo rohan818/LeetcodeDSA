c3aeb92959c4709cf677cb47d31307f1386aa74c
rohan818 <64457330+rohan818@users.noreply.github.com>
2025-08-10 20:23:52 +0530

    code 217

--- Arrays/217. Contains Duplicate.py
+++ Arrays/217. Contains Duplicate.py
@@ -1,19 +1,10 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hash = {}
-        for i in range(len(nums)):
-            if (nums[i] in hash) and (abs(i-hash[nums[i]]) <= k):
-                return True
-            hash[nums[i]] = i
-        return False
-        '''        
-        num_dict = {}
-        for i in nums:
-            if i in num_dict:
-                return True
-            else:
-                num_dict[i] = 1
-        
-        return False
-        num_dict = {}
-        '''
+#
+def containsDuplicate(self, nums: List[int]) -> bool:
+    num_dict = {}
+    for i in nums:
+        if i in num_dict:
+            return True
+        else:
+            num_dict[i] = 1
+    
+    return False
