b0e180d27f72928078ffee8b1935a310aae9d2b8
Rohan <64457330+rohan818@users.noreply.github.com>
2025-08-24 20:41:55 +0530

    Updated 415

--- Strings/415. Add Strings.py
+++ Strings/415. Add Strings.py
@@ -1,6 +1,8 @@
-#
+import sys
+
 def addStrings(self, num1: str, num2: str) -> str:
     def str2int(num):
+        sys.set_int_max_str_digits(10000)
         numDict = {'0' : 0, '1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5,'6' : 6, '7' : 7, '8' : 8, '9' : 9}
         output = 0
         for d in num:
