5afcf472bb48c12cbd5b8b061f3e1555d514fa97
Rohan <64457330+rohan818@users.noreply.github.com>
2025-09-27 17:03:08 +0530

    code 1512

--- Arrays/1512. Number of Good Pairs.py
+++ Arrays/1512. Number of Good Pairs.py
@@ -4,8 +4,6 @@ def numIdenticalPairs(self, nums: List[int]) -> int:
     c=0
     for i in range(0,l-1):
         for j in range(i,l):
-           #print(i,nums[i],j,nums[j])
             if nums[i]==nums[j] and i<j:
-                #print('*')
                 c+=1
     return c
