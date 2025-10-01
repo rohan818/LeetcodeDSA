1e36918c704a55396f4c1a2d7dec96bb75b57e38
Rohan <64457330+rohan818@users.noreply.github.com>
2025-10-01 21:42:06 +0530

    code 1920

--- Arrays/1920. Build Array from Permutation.py
+++ Arrays/1920. Build Array from Permutation.py
@@ -5,3 +5,11 @@ def buildArray(self, nums: List[int]) -> List[int]:
     for i in range(n):
         r.append(nums[nums[i]])
     return r
+
+'''
+from typing import List
+
+def buildArray(self, nums: List[int]) -> List[int]:
+    return [nums[nums[i]] for i in range(len(nums))]
+
+'''
