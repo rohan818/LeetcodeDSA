c2b19170b24e24638f78f74e5d368ea9dc51525a
Rohan <64457330+rohan818@users.noreply.github.com>
2025-10-07 20:54:47 +0530

    code 821

---
+++ Strings/821. Shortest Distance to a Character.py
@@ -0,0 +1,16 @@
+#
+def shortestToChar(self, s: str, c: str) -> List[int]:
+    occurace_of_c = []
+    result = []
+
+    for i in range(len(s)):
+        if s[i] == c:
+            occurace_of_c.append(i)
+
+    for i in range(len(s)):
+        check=[]
+        for j in occurace_of_c:
+            check.append(abs(i-j))
+        result.append(min(check))
+
+    return result
