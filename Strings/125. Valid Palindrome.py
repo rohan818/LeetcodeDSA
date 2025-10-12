63b5a2d9a9173a1cd554ed77e9af1acdb3bdbdf6
Rohan <64457330+rohan818@users.noreply.github.com>
2025-10-12 21:35:32 +0530

    Upd 125

--- Strings/125. Valid Palindrome.py
+++ Strings/125. Valid Palindrome.py
@@ -3,3 +3,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = re.sub(r'[^a-z0-9]+', '', s.lower())
         return s == s[::-1]
+
+    # s = ''.join(c.lower() for c in s if c.isalnum())
+    # return s == s[::-1]
