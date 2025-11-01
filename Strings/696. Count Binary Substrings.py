cdfa7ccfa19a9d8c1b9f1f485355fb7e5d134b5d
Rohan <64457330+rohan818@users.noreply.github.com>
2025-11-01 18:55:16 +0530

    code 696

---
+++ Strings/696. Count Binary Substrings.py
@@ -0,0 +1,14 @@
+#
+def countBinarySubstrings(self, s: str) -> int:
+    groups = [1]
+    for i in range(1, len(s)):
+        if s[i-1] != s[i]:
+            groups.append(1)
+        else:
+            groups[-1] += 1
+        #print(i, groups)
+
+    ans = 0
+    for i in range(1, len(groups)):
+        ans += min(groups[i-1], groups[i])
+    return ans 
