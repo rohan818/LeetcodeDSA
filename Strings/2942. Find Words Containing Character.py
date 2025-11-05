99582478562b31664daac3aadacbc0ccfedcfed7
Rohan <64457330+rohan818@users.noreply.github.com>
2025-11-05 21:12:29 +0530

    Upd 2942

--- Strings/2942. Find Words Containing Character.py
+++ Strings/2942. Find Words Containing Character.py
@@ -6,3 +6,5 @@ def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         if x in words[i]:
             res.append(i)
     return res
+
+    #return [i for i, w in enumerate(words) if x in w]
