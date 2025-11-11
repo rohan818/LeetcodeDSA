fb23ad52a3487064a6e5aaaf89b09bf756f5a7e3
Rohan <64457330+rohan818@users.noreply.github.com>
2025-11-11 21:20:18 +0530

    code 2706

---
+++ Arrays/2706. Buy Two Chocolates.py
@@ -0,0 +1,8 @@
+#
+def buyChoco(self, prices: List[int], money: int) -> int:
+    prices.sort()
+    min_cost = prices[0] + prices[1]
+
+    if min_cost <= money:
+        return money - min_cost
+    return money
